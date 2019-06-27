6157b49c1879f377cc21e2e2138ab24f8b762c49
Max <skridlevsky@gmail.com>
2019-06-27 18:01:47 +0300

    System health page initial implementation

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    { id: 1, customer: {id: 1, name: 'Maxima', country: 'Latvia', email: 'info@maxima.lv'}, total: 230, placed: new Date(2019,6,1), fulfilled: new Date(2019,6,2)},
+    { id: 1, customer: {id: 1, name: 'Maxima', country: 'Latvia', email: 'info@maxima.lv'}, total: 230, placed: new Date(2019,6,1), fulfilled: new Date(2019,6,2)},
+    { id: 1, customer: {id: 1, name: 'Maxima', country: 'Latvia', email: 'info@maxima.lv'}, total: 230, placed: new Date(2019,6,1), fulfilled: new Date(2019,6,2)},
+    { id: 1, customer: {id: 1, name: 'Maxima', country: 'Latvia', email: 'info@maxima.lv'}, total: 230, placed: new Date(2019,6,1), fulfilled: new Date(2019,6,2)},
+    { id: 1, customer: {id: 1, name: 'Maxima', country: 'Latvia', email: 'info@maxima.lv'}, total: 230, placed: new Date(2019,6,1), fulfilled: new Date(2019,6,2)}
+  ];
 
   ngOnInit() {
   }
